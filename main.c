e92ed3c29a224ede5e6198acd122a33b785a10e8
Pratiksha1411 <97377782+Pratiksha1411@users.noreply.github.com>
2022-02-09 19:41:37 +0530

    check palindrom or not

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 main()
 {
-
-    int num,d1,d2,sum;
-    printf("enter three digit number");
+    int num,sum,ori,d1,d2;
+    printf("enter the 3 digit number");
     scanf("%d",&num);
+    ori=num;
     d1=num%10;
     num=num/10;
     d2=num%10;
     num=num/10;
     sum=d1*100+d2*10+num;
-    printf("reverse of number=%d",sum);
+    if(sum==ori)
+    {
+        printf("number is palindrom");
+    }
+    else
+    {
+        printf("number is not a palindrom");
+    }
     getch();
-
 }
