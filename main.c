223a75e3c83efeecea7768372d055ed6b4c47e2c
Pratiksha1411 <97377782+Pratiksha1411@users.noreply.github.com>
2022-02-09 19:43:06 +0530

    reverse three digit number

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 main()
 {
-    int num,sum,ori,d1,d2;
-    printf("enter the 3 digit number");
+
+    int num,d1,d2,sum;
+    printf("enter three digit number");
     scanf("%d",&num);
-    ori=num;
     d1=num%10;
     num=num/10;
     d2=num%10;
     num=num/10;
     sum=d1*100+d2*10+num;
-    if(sum==ori)
-    {
-        printf("number is palindrom");
-    }
-    else
-    {
-        printf("number is not a palindrom");
-    }
+    printf("reverse of number=%d",sum);
     getch();
+
 }
